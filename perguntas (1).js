e5f0a9a2ea6040701c67a7cacdb1c27985c846c5
pedrogustvo <162698272+pedrogustvo@users.noreply.github.com>
2024-11-22 21:04:27 -0300

    Add files via upload

---
+++ perguntas (1).js
@@ -0,0 +1,23 @@
+criarCartao(
+'Programação',
+'O que é JavaScript?',
+'O JavaScript é uma linguagem de programação'
+)
+
+criarCartao(
+'Geografia',
+'Qual a capital da França?',
+'A capital da França é Paris'
+)
+
+criarCartao(
+'Programação',
+'O que é uma função?',
+'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criarCartao(
+'Língua Inglesa',
+'Como se diz oi em Inglês?',
+'Oi em inglês se diz HI (RAI)'
+)
